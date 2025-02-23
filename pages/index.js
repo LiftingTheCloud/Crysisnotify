efeb21d67295f24df73acdd1792a9e64d6b5e484
LiftingTheCloud <watcharaphon0630374165@gmail.com>
2025-02-23 15:31:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [weather, setWeather] = useState(null);
-  
+
   useEffect(() => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=a18ceb9f48a77d08cf51731b1e25ec8f&units=metric`
